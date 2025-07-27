eef46654577fe982fb822dc3e3e0bb0ae7cb0623
Mehrab-Abir <mjabir@myseneca.ca>
2025-07-27 17:43:23 -0400

    Responsive menu

---
+++ script.js
@@ -0,0 +1,6 @@
+function openSlideMenu(){
+    document.getElementById("side-nav").style.width = "200px";
+}
+function closeSideMenu(){
+    document.getElementById("side-nav").style.width = "0";
+}
